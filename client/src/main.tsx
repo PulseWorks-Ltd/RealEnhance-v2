3115a9dc07d478670414b8c502cf4b16dcb0fe51
PulseWorks-Ltd <hello@pulseworks.co.nz>
2025-11-01 07:47:47 +0000

    Client Update 2.1

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,26 +1,24 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "@/context/AuthContext";
 import App from "./App";
 import "./index.css";
-import { Toaster } from "@/components/ui/toaster";
-
-const rootEl = document.getElementById("root");
-if (!rootEl) throw new Error('#root not found in index.html');
 
 const qc = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
 });
 
-createRoot(rootEl).render(
+const rootEl = document.getElementById("root");
+if (!rootEl) throw new Error('Root element "#root" not found');
+
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
     <QueryClientProvider client={qc}>
       <AuthProvider>
         <BrowserRouter>
           <App />
-          <Toaster />
         </BrowserRouter>
       </AuthProvider>
     </QueryClientProvider>
