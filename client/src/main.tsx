7235f5e84250b367300db7b621af556e0a2dbd02
PulseWorks-Ltd <hello@pulseworks.co.nz>
2025-11-01 08:24:56 +0000

    tsconfig update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "@/context/AuthContext";
+import { Toaster } from "@/components/ui/toaster";
 import App from "./App";
-import "./index.css";
+import "@/index.css";
 
 const qc = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
@@ -13,14 +14,16 @@ const qc = new QueryClient({
 const rootEl = document.getElementById("root");
 if (!rootEl) throw new Error('Root element "#root" not found');
 
-ReactDOM.createRoot(rootEl).render(
+createRoot(rootEl).render(
   <React.StrictMode>
     <QueryClientProvider client={qc}>
       <AuthProvider>
         <BrowserRouter>
           <App />
+          <Toaster />
         </BrowserRouter>
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
+
