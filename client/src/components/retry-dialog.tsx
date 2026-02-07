9611dd010896e980a620469bb1c8a28e007ce576
PulseWorks-Ltd <hello@pulseworks.co.nz>
2026-02-07 19:08:34 +0000

    Icon Import Fix

--- client/src/components/retry-dialog.tsx
+++ client/src/components/retry-dialog.tsx
@@ -6,6 +6,7 @@ import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
+import { Info } from "lucide-react";
 
 
 interface RetryDialogProps {
